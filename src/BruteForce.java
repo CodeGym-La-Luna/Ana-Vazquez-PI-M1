a440733e95ca1b2f3498ee1a44e9247e654edb16
anivaz <anivaz88@outlook.com>
2024-09-27 21:02:38 -0500

    feat: eliminar archivos txt, fix fuerza bruta y filemanager

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,17 +1,18 @@
 public class BruteForce {
 
+
     // Descifrar el texto cifrado-fuerza bruta
     public String decryptByBruteForce(String encryptedText, char[] alphabet) {
         Cifrado cifrado = new Cifrado(alphabet);
-        StringBuilder allAttempts = new StringBuilder();
+        StringBuilder combinaciones = new StringBuilder();
 
         // Probar todas las claves posibles
-        for (int i = 1; i < alphabet.length; i++) {
-            String attempt = cifrado.decrypt(encryptedText, i);
-            allAttempts.append("Clave ").append(i).append(": ").append(attempt).append("\n");
+        for (int i = 0; i < alphabet.length; i++) {
+            String combinacion = cifrado.decrypt(encryptedText, i);
+           combinaciones.append("Desplazamiento").append(i).append(": ").append(combinacion).append("\n");
         }
 
         // Devolver todos los intentos para revision
-        return allAttempts.toString();
+        return combinaciones.toString();
     }
 }
